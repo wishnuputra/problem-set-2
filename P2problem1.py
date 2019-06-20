dc78df6cba3d0bcc5a49dcf43ab7f20904d07c07
Wishnuputra Dhanu <wishnuputra.dhanu@gmail.com>
2019-06-20 11:57:13 +0700

    Add solution to Pset2

---
+++ P2problem1.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+=========================================================
+PSET2: Problem 1
+MITx 6.00.1x
+=========================================================
+
+This is a program for problem set 2, problem 1 of
+MITx 6.00.1x
+
+This program calculates the credit card balance
+after one year if a person only pays the minimum monthly
+payment required by the credit card company each month.
+
+The following variables contain values as described
+below:
+
+balance - the outstanding balance on the credit card
+annualInterestRate - annual interest rate as a decimal
+monthlyPaymentRate - minimum monthly payment rate as a decimal
+
+At the end of 12 months, it will print out the remaining
+balance with two decimal digits of accuracy.
+
+A summary of the required math is found below:
+
+Monthly interest rate= (Annual interest rate) / 12.0
+Minimum monthly payment = 
+(Minimum monthly payment rate) x (Previous balance)
+
+Monthly unpaid balance =
+(Previous balance) - (Minimum monthly payment)
+
+Updated balance each month = 
+(Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+
+Created on Wed Jun 19 18:26:22 2019
+
+@author: Wishnuputra
+"""
+
+balance = 42
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
+
+for i in range(12):
+    unpaidBal = balance - balance*monthlyPaymentRate
+    balance = unpaidBal + unpaidBal*annualInterestRate / 12
+    
+balance = round(balance, 2)
+print("Remaining balance:", balance)
---
+++ P2problem3.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+=========================================================
+PSET2 Problem 3 - Paying Debt Off In A Year
+(Bisection Search)
+
+MITx 6.00.1x
+=========================================================
+
+This program calculates the minimum fixed monthly payment
+needed in order pay off a credit card balance within
+12 months. By a fixed monthly payment, a constant amount
+that will be paid each month.
+
+This program uses bisection search to find the smallest
+monthly payment to the cent (no more multiples of $10)
+such that we can pay off the debt within a year. This
+program runs much faster than the problem 2 which uses
+linear search.
+
+Lower and upeer bound for bisection search:
+
+Monthly interest rate = (Annual interest rate) / 12.0
+Monthly payment lower bound = Balance / 12
+Monthly payment upper bound = (Balance x (1 + Monthly interest rate)) / 12.0
+
+Created on Thu Jun 20 09:26:31 2019
+
+@author: Wishnuputra
+"""
+
+balance = 999999
+annualInterestRate = 0.18
+
+cents = -0.01
+remBal = balance
+low = balance/12
+high = (balance * (1 + annualInterestRate)) / 12.0
+
+while True:
+    ans = (high + low) / 2.0
+
+    # calculate remaining balance after one year
+    for i in range(12):
+        unpaidBal = remBal - ans
+        remBal = unpaidBal + unpaidBal*annualInterestRate / 12.0
+
+    # check if the remaining balance has met the cents accuracy
+    if  remBal < 0 and remBal > cents:
+        break
+    else:
+        if remBal > 0:
+            low = ans
+        elif remBal < 0:
+            high = ans
+        remBal = balance
+
+print("Lowest Payment:", round(ans, 2))
